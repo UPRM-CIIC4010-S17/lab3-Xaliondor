199d72f081977d359149e10fa858636013a41d89
4010f6 <4010f6@nispero.ece.uprm.edu>
2017-02-08 16:17:54 -0400

    Commit to local repository

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("GitHub Primer");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 200);
+		JFrame myFrame = new JFrame("Sebastian Rodriguez");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(860, 390);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
