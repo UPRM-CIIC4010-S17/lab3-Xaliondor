9a0256893af935441076565149ac4cb296b1bed0
4010f6 <4010f6@nispero.ece.uprm.edu>
2017-02-08 16:20:15 -0400

    Commit to remote repository

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Sebastian Rodriguez");
+		JFrame myFrame = new JFrame("SebastianRodriguez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(860, 390);
 		myFrame.setSize(200, 200);
