e0614c426b929293893a476c0098e8034f0c307d
4010a9 <4010a9@tamarindo.ece.uprm.edu>
2017-11-15 16:56:48 -0400

    Question 7

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Insets;
+import java.awt.Polygon;
+
 import javax.swing.JPanel;
  
 public class MyPanelClass extends JPanel {
@@ -24,10 +26,28 @@ public class MyPanelClass extends JPanel {
                         //Paint the background
                         g.setColor(Color.GRAY);
                         g.fillRect(x1, y1, width+1, height+1);
-                        //Draw a border
-                        g.setColor(Color.BLACK);
-                        g.drawRect(x1, y1, width, height);
-                        g.setColor(Color.DARK_GRAY);
-                        g.drawRect(x1+12, y1+12, width-24, height-24);
-            }
+//                        //Draw a border
+//                        g.setColor(Color.BLACK);
+//                        g.drawRect(x1, y1, width, height);
+//                        g.setColor(Color.DARK_GRAY);
+//                        g.drawRect(x1+12, y1+12, width-24, height-24);
+//                        //Draw Lines
+//                        g.setColor(Color.WHITE);
+//                        g.drawLine(x1, y1, x2, y2);
+//                        g.setColor(Color.RED);
+//                        g.drawLine(x1, y1+height, x2, y2-height);
+                        //DrawOval
+//                        g.setColor(Color.LIGHT_GRAY);
+//                        g.fillOval((x2/2) - (55/2), (y2/2) - (55/2), 55, 55);
+                        Polygon p = new Polygon();
+                        p.addPoint(x1 + 5, y1 + 25);
+                        p.addPoint(x1 + 20, y1 + 10);
+                        p.addPoint(x1 + 35, y1 + 25);
+                        p.addPoint(x1 + 25, y1 + 25);
+                        p.addPoint(x1 + 25, y1 + 45);
+                        p.addPoint(x1 + 15, y1 + 45);
+                        p.addPoint(x1 + 15, y1 + 25);
+                        g.setColor(Color.YELLOW);
+                        g.drawPolygon(p);
+			}
 }
